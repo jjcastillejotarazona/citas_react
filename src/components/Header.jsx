6e86d59da79222a381bf9d8a297364fdf97850c1
José castillejo <jjcastillejotarazona@gmail.com>
2022-05-01 12:10:57 -0500

    text add Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header(){
     const variableHeader = true;
     return (
         <h1 className="font-black text-5xl w-1/2 mx-auto md:w-2/3 text-center">
-            Seguimienyo pacientes {''}
+            Seguimienyo pacientes {''} ART-TEC {""}
             <span className="text-indigo-600">Veterinaria</span>
         </h1>
     )
