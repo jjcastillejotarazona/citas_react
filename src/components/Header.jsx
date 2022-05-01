4d3e3be7544305f531bf6721c29c3ac80909e9e1
José castillejo <jjcastillejotarazona@gmail.com>
2022-05-01 11:23:17 -0500

    Commit first

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header(){
+
+    const variableHeader = true;
+    return (
+        <h1 className="font-black text-5xl w-1/2 mx-auto md:w-2/3 text-center">
+            Seguimienyo pacientes {''}
+            <span className="text-indigo-600">Veterinaria</span>
+        </h1>
+    )
+}
+
+export default Header;
